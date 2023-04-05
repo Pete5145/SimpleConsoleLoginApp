ee0592a1f8d052d72fc0f04ba19955b10ac26e8d
Pete5145 <peterosakpolor661@gmail.com>
2023-04-05 15:43:59 -0700

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userNames = { "Pete524", "James242", "Stuart535"};
+            string[] passWords = { "51450", "42942", "76931" };
+
+            SimpleLoginApp loginApp = new SimpleLoginApp(userNames, passWords);
+
+            loginApp.PromptUser();
+        }
+    }
+}
---
+++ ConsoleApp5/SimpleLoginApp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class SimpleLoginApp
+    {
+       private string[] usernames;
+       private string[] passwords;
+
+       public SimpleLoginApp(string[] username, string[] password) 
+       {
+            this.usernames = username; 
+            this.passwords = password;
+       }
+       
+       public void PromptUser()
+       {
+            Console.WriteLine("==========Welcome To My App==========");
+
+            Console.Write("Enter your username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                if (username == usernames[i])
+                {
+                    for (int j = 0; j < passwords.Length; j++)
+                    {
+                        if (password == passwords[j])
+                        {
+                            if (i == j)
+                            {
+                                Console.WriteLine("Login Successful!");
+                                return;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Login Denied!");
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
